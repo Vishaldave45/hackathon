999330eb9a59458e8f44bbe536b723b3d27e5e5c
Vishaldave45 <125914169+Vishaldave45@users.noreply.github.com>
2025-02-17 12:08:31 +0530

    Add files via upload

---
+++ code/script.js
@@ -0,0 +1,12 @@
+// Add interactivity if needed
+document.querySelector('.banner-btn').addEventListener('click', () => {
+    alert('Learn more about our platform!');
+  });
+  
+  document.querySelector('.farmer-btn').addEventListener('click', () => {
+    alert('Join as a farmer!');
+  });
+  
+  document.querySelector('.shop-btn').addEventListener('click', () => {
+    alert('Start shopping now!');
+  });
